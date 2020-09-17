5b5040496341218cd10b6e2f1bf01da23f55818a
Ben Dosch <Benjamindosch@gmail.com>
2020-09-17 21:52:12 +0000

    if added to loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,21 @@
 int main(void)
 {
 	int i;
-
+	int j;
 	for (i = '0' ; i <= '9' ; i++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if ((i+j) == ('9'+'9'))
+				continue;
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
