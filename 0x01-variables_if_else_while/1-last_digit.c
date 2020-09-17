36513b1676b15ca5100178ff86d12f7277d7c3c0
Ben Dosch <Benjamindosch@gmail.com>
2020-09-17 19:04:40 +0000

    abs() added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	is = n % 10;
+	is = abs(n % 10);
 	printf("Last digit of %d is ", n);
 	if (is > 5)
 		printf("%d and is greater than 5\n", is);
