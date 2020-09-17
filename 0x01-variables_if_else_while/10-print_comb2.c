bada4feb993a3f2d2ca5b94c9790c89087a24315
Ben Dosch <Benjamindosch@gmail.com>
2020-09-17 21:59:07 +0000

    9 sent to 10, the where it should have been

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -12,13 +12,20 @@ int main(void)
 	int j;
 
 	for (i = '0' ; i <= '9' ; i++)
+	{
 		for (j = '0'; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
-			putchar(',');
-			putchar(' ');
+			if ((i + j) == ('9' + '9'))
+				continue;
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
