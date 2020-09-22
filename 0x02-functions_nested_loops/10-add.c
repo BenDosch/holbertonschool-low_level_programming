1e3b9ca012475e17829edb8d710d640b93279ae0
Ben Dosch <Benjamindosch@gmail.com>
2020-09-22 02:30:59 +0000

    10 added, betty, tested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds 2 integers and returns the result
+ * @i: first integer
+ * @j: second integer
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
