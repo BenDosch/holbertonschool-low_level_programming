28d3a0a8951d8f49b58a21879589535a4eb6bca0
Ben Dosch <Benjamindosch@gmail.com>
2020-09-22 02:22:31 +0000

    9 added, betty, tested

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+	char tens, ones;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			tens = '0' + (k / 10);
+			ones = '0' + (k % 10);
+			if (j == 9 && tens == '0')
+				_putchar(ones);
+			else if (j == 9 && tens != '0')
+			{
+				_putchar(tens);
+				_putchar(ones);
+			}
+			else if (tens == '0' && (i * (j + 1)) / 10 != 0 )
+			{
+				_putchar(ones);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (tens == '0' && (i * (j + 1) / 10 == 0))
+			{
+				_putchar(ones);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(tens);
+				_putchar(ones);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
