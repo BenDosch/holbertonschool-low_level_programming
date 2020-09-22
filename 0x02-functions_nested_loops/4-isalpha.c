3b69c5c98301e24958d004ddc984016de4e6e76d
Ben Dosch <Benjamindosch@gmail.com>
2020-09-22 14:25:27 +0000

    second bol corrected

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * isalpha - checks for alphabetic character.
  *
@@ -6,7 +7,7 @@
 
 int _isalpha(int c)
 {
-	if ( (c >= 'a' && c<= 'z') || (c <= 'A' && c >= 'Z'))
+	if ( (c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
