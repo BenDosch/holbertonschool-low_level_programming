32e872a5b3a9b8dbbd2b94add23431d76701a10b
Ben Dosch <Benjamindosch@gmail.com>
2020-09-22 14:38:27 +0000

    betty corrected

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- *print_last_diget - prints the last diget of a number
+ * print_last_diget - prints the last diget of a number
  * @n: number passed
- * @d: last diget of n
- *Return: d last diget
+ *
+*Return: d last diget
  */
 
 int print_last_digit(int n)
