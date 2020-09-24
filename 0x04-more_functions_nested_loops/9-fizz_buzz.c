a481da685cb419e92f94c6bf4b7a2637d2160c3a
Ben Dosch <Benjamindosch@gmail.com>
2020-09-24 21:32:55 +0000

    9 function/main added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - preforms the fizzbuzz test
+ *
+ * Return: none
+ */
+
+void fizz_buzz(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("Fizzbuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num <= 100)
+			printf(" ");
+	}
+}
+
+/**
+ * main - calls fizz_buzz
+ *
+ * Return: 0 Allways, sucess
+ */
+
+int main(void)
+{
+	fizz_buzz();
+
+	return (0);
+}
