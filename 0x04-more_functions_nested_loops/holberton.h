1b9e88cd6be7be4ba0482e4a2c4d22d790755e79
Ben Dosch <Benjamindosch@gmail.com>
2020-09-24 19:17:23 +0000

    0 now betty and tested

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,8 +2,8 @@
 #define HEADER_FILE
 
 int _isupper(int c);
-int _isdigit(int c):
-int mul(int a, intb);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
