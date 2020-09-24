718f9d0e55ecabf33a1b3cccb1e84611dc3258d5
Ben Dosch <Benjamindosch@gmail.com>
2020-09-24 20:50:04 +0000

    7 main and fucntion added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line n long
+ * @n: lenght of line
+ * Return: none
+ */
+
+void print_diagonal(int n)
+{
+	int row, col;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row <= n; row++)
+		{
+			if (row != 0)
+			{
+				for (col = 0; col <= (row - 1); col++)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
