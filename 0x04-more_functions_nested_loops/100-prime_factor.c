2ff5c2d0114cf14c6548da5d0f0b0d871854c210
Ben Dosch <Benjamindosch@gmail.com>
2020-09-24 23:41:10 +0000

    100 added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - gets largest prime factor of num
+ *
+ * Return: Always 0, sucess
+ */
+
+int  main(void)
+{
+	long long int num, fac, maxfac;
+
+	num = 612852475143
+	if (num >= 0)
+	{
+		for (fac = 2; num / maxfac == 1;)
+		{
+			if (num % fact != 0)
+			{
+				fac++;
+			}
+			else
+			{
+				maxfac = fac;
+				num = num / fac;
+			}
+		}
+		printf("%lld\n", maxfac);
+	}
+	return (0);
+}
