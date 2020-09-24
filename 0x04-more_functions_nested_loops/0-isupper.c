775739c0bb17343912acc4ffaeb3487b6679c362
Ben Dosch <Benjamindosch@gmail.com>
2020-09-24 18:45:14 +0000

    directory added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _isupper - checks if c is uppercase
+* @c: intiger passed to function representing a character
+* Return: 1 if upper, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+  if ((c >= 'A') || (c <= 'Z'}}
+  {
+    return (1);
+  }
+  else
+    {
+    return (o);
+    }
+{
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+#0x04-more_functions_nested_loops
+
+##Intro
+
+Project directory for holberton focused on loops and functions.
