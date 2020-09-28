c6329d23ab1c428d19ff1d3a406b770548cda8ba
Ben Dosch <Benjamindosch@gmail.com>
2020-09-28 21:45:55 +0000

    7 added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: string passed to fucntion
+ * Return: none
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+	}
+	if (i % 2 != 0)
+	{
+		i--;
+	}
+	for (i = i / 2; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
