2838649eb5ae8d26d17eb62201f6169639511cbc
Ben Dosch <Benjamindosch@gmail.com>
2020-09-28 21:54:14 +0000

    8 added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of aray a
+ * @a: array passed to function
+ * @n: number of elements to be printed
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", *a);
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", *(a + i));
+	}
+	printf("\n");
+}
