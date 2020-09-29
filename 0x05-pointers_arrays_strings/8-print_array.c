fe8bf7f93914d7d7bced4a0e87d0b72f8adab264
Ben Dosch <Benjamindosch@gmail.com>
2020-09-29 15:59:55 +0000

    accounting for 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	printf("%d", *a);
-	for (i = 1; i <= n; i++)
+	if (n != 0)
+		printf("%d", *a);
+	for (i = 1; i < n; i++)
 	{
 		printf(", %d", *(a + i));
 	}
