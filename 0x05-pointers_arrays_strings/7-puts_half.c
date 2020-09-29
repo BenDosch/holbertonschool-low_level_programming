ce7db0273e75d0bf1fbba8b5a727755035487a36
Ben Dosch <Benjamindosch@gmail.com>
2020-09-29 15:38:18 +0000

    correctly prings second half of odds

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,13 @@ void puts_half(char *str)
 	for (i = 0; *(str + i) != '\0'; i++) /* get string length*/
 	{
 	}
-	if ((i % 2 != 0) && (i > 1)) /* if odd, reduce length by 1 before deviding.*/
+	/* if odd, reduce length by 1 before deviding.*/
+	if ((i % 2 != 0) && (i > 1))
 	{
-		i--;
+		i++;
 	}
-	/* devide by 2 and increment up to the null while printing */
-	for (i = i / 2; *(str + i) != '\0'; i++)
+	/* devide by 2 and increment until the null while printing */
+	for (i = (i / 2) ; *(str + i) != '\0'; i++)
 	{
 		_putchar(*(str + i));
 	}
