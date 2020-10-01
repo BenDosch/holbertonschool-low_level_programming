cd5cbed9ebdb13c7e8472fd52bbbca38c6926bcf
Ben Dosch <Benjamindosch@gmail.com>
2020-10-01 16:53:43 +0000

    4 added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverese the contenst of an array of n intigers
+ * @a: array to be reversed
+ * @n: lenght of a
+ * Return: none
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int tem;
+
+/*Loop through half of the array*/
+	for (i = 0; i <= (n - 1) / 2; i++)
+	{
+/*Store lower value in tem and assign swaped values*/
+		tem = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = tem;
+	}
+}
