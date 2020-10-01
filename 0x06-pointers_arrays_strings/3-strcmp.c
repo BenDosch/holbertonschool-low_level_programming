0ac9269c2364f5f619e574a45c709d2fe6564c46
Ben Dosch <Benjamindosch@gmail.com>
2020-10-01 18:50:01 +0000

    adjusted for ascii diffrence

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int cmp;
 
 /*Compare s1 to s2*/
-	for (i = 0; s1[i] == s2[i]; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		cmp = 0:
-		break;
-	}
-	}
 	if (s1[i] > s2[i])
-		cmp = 1;
+		return (s1[i] - s2[i]);
 	else if (s1[i] < s2[i])
-			cmp = -1;
-/*Return -1 if s1 < s2, 0 if s1 = s2, and 1 if s1 > s2*/
-	return (cmp);
+		return (s1[i] - s2[i]);
+	}
+	return (0);
 }
