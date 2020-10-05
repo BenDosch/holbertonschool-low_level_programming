59a4c6649f9ecf62eadea8a3cd851c1fbed592b7
Ben Dosch <Benjamindosch@gmail.com>
2020-10-05 20:36:42 +0000

    7 added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,21 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_chess - prints a chess board
+ * @a: 2d array containg chessboard
+ * Return: none
  */
 
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0, j = 0; i < 8 || j < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
