e3aaaf114a377760b3a9a44eeedcc4f3af450cb8
Ben Dosch <Benjamindosch@gmail.com>
2020-10-05 18:25:40 +0000

    betty fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _strchr - finds the location of the first occurance of c
+ * @s: sting to find c in
+ * @c: character to find
+ * Return: pointer to location of c
  */
 
 char *_strchr(char *s, char c)
@@ -13,7 +14,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		    return (s + i);
+			return (s + i);
 	}
 		return ('\0');
 }
