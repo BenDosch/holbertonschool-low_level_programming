156ecd3fc3e6ca8977917d828badda891add56b6
Ben Dosch <Benjamindosch@gmail.com>
2020-10-06 00:16:34 +0000

    9 added

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,8 +1,13 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * set_string - set the value of a pointer to a character
+ * @s: string to be changed
+ * @to: string to change to
+ * Retrun: none
  */
 
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
