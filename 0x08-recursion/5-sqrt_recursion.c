fb4d1662582e063684369574f5b1799fcabf2925
Ben Dosch <Benjamindosch@gmail.com>
2020-10-07 17:58:49 +0000

    5 ready to test

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
 
 /**
+ * test_root - recursively tests if x is the square root of n
+ * @n: number to get square root of
+ * @x: number to test if root
+ * Return: x if root of n, -1 if error
+ */
+
+int test_root(int n, int x)
+{
+	x--;
+
+	if (x * x == n)
+		return (x);
+	else if (x >= 1)
+	{
+		x = test_root(n, x);
+		return (x);
+	}
+
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - returns the square root of a number only if natural
  * @n: number to get square root of
  * Return: square root of n, -1 if error
@@ -8,6 +30,8 @@
 
 int _sqrt_recursion(int n)
 {
-	
+	int root;
 
+	root = test_root(n, n);
+	return (root);
 }
