bd92411f4f71a7734047450b400278e131b983d0
Ben Dosch <Benjamindosch@gmail.com>
2020-10-09 20:42:48 +0000

    A added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - whole program that prints the name of the program.
+ * @argc: number of arguments passed to c
+ * @argv: array of arguments passed to main
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - program that prings the value of argc
+ * @argc: number of arguments passed to program
+ * @argv: arguments passed to program
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+	return (0);
+}
