7bfe66a6033f52cf6d1519bf431038a79fba89a2
Ben Dosch <Benjamindosch@gmail.com>
2020-10-13 22:36:30 +0000

    5 ready to test

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -7,7 +7,36 @@
  *
  */
 
-int main()
+char *argstostr(int ac, char **av)
 {
+	int i, j, k = 0;
+	char *new;
+	int lennew;
 
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			lennew++;
+		}
+		lennew++;
+	}
+	new = malloc(sizeof(char) * (lennew + 1));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			new[k] = av[i][j];
+			k++;
+		}
+		new[k] = '\n';
+		k++;
+	}
+	new[k] = '\0';
+	return (new);
 }
