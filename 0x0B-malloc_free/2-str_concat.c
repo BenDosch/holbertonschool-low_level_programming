0cd70c2efbd8ddaa3e5b770b7ca29e0b691c022f
Ben Dosch <Benjamindosch@gmail.com>
2020-10-13 19:06:53 +0000

    accounting for null strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1, len2, i;
 	char *combo;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		*s1 = 0;
+	if (s2 == NULL)
+		*s2 = 0;
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
