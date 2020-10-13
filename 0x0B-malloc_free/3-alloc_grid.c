b647edafba59576dab7f0c0b9ad97380b3cca64f
Ben Dosch <Benjamindosch@gmail.com>
2020-10-13 21:22:17 +0000

    3 ready to test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,18 +11,21 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **grid;
 	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int) * height * width);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			return (NULL);
 		for (j = 0; j < width; j++)
-			*(grid + (i * width) + j) = 0;
-	}
+			grid[i][j] = 0;
+ 	}
 	return (grid);
 }
