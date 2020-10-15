444272f86f86fbbd0a6aa8ac0d0a42dd0c13da76
Ben Dosch <benjamindosch@gmail.com>
2020-10-15 18:58:06 +0000

    sizeof removed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
