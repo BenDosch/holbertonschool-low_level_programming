cb5a443f722a1a88fe980cfcc28057c58d4784d9
Ben Dosch <Benjamindosch@gmail.com>
2020-10-19 16:29:00 +0000

    all ready for checker

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of the file is was complied from
+ * Return: Always 0
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
