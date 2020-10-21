40e941ae092f60ca63ee6802307d733a27ba7b3c
Ben Dosch <Benjamindosch@gmail.com>
2020-10-21 15:47:18 +0000

    0 ready for testing

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to name to be printer
+ * @f: pointer to function to be applied to the name to be printed
+ * Return: none
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		*name = "";
+	if (f == NULL)
+		return;
+	f(name);
+}
