2d1fb1c1ea24b526dd1bff3966f6560d8d758e35
Ben Dosch <Benjamindosch@gmail.com>
2020-10-29 20:13:35 +0000

    0 ready to test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,27 @@
 #include "lists.h"
 
 /**
- * print_list - prints a list
+ * print_list - prints a string and its lenght in a linked list
  * @h: list to print
  * Return: the size of the list
  */
 
 size_t print_list(const list_t *h)
 {
-	if
+	unsigned long int i;
+
+	if (h->next == NULL)
+		return (0);
+	for (i = 0; h->next == NULL; i++, h = h->next)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+	}
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%lu] %s\n", h->len, h->str);
+	return (i);
 }
