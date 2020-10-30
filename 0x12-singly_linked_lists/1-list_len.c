033532fcf2f5d49d7135c3a78797606cedcd9349
Ben Dosch <Benjamindosch@gmail.com>
2020-10-30 17:15:48 +0000

    betty fixes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,18 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * list_len - returns the lenght of a list
+ * @h: list to get lenght of.
+ * Return: the size of the list
  */
 
+size_t list_len(const list_t *h)
+{
+	unsigned long int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
+}
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!n_node)
 	{
 		free(n_node);
-		return(NULL);
+		return (NULL);
 	}
 	n_node->str = strdup(str);
 	if (n_node->str == NULL)
