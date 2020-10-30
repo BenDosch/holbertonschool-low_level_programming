422961fcb2313f4f147389a29e80ece1bf839336
Ben Dosch <Benjamindosch@gmail.com>
2020-10-30 17:04:22 +0000

    now outputs

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,16 +14,21 @@ list_t *add_node(list_t **head, const char *str)
 
 	n_node = malloc(sizeof(list_t));
 	if (!n_node)
+	{
 		free(n_node);
+		return(NULL);
+	}
 	n_node->str = strdup(str);
 	if (n_node->str == NULL)
 	{
 		free(n_node->str);
 		free(n_node);
+		return (NULL);
 	}
 	for (i = 0; n_node->str[i] != '\0'; i++)
 	{}
 	n_node->len = i;
 	n_node->next = *head;
+	*head = n_node;
 	return (n_node);
 }
