01ba472e4bbf29e2217a6fd24d20cd85d894109c
Ben Dosch <Benjamindosch@gmail.com>
2020-10-30 21:55:31 +0000

    100 ready

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void print_first(void)__attribute__((constructor));
+
+/**
+ * print_first - prints a string before main
+ * Return: none
+ */
+
+void print_first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
