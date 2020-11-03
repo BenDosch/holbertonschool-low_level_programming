65a21ecd9560c9eb2d744360aecf5010c863ae52
Ben Dosch <Benjamindosch@gmail.com>
2020-11-03 22:35:00 +0000

    4 and 5 ready

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list then sets head to NULL
+ * @head:head of list to free
+ * Return: none
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	while(*head != NULL)
+	{
+		next = (**head).next;
+		free(*head);
+		*head = next;
+	}
+	head = NULL;
+}
