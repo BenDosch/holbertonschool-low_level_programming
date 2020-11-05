566c52e5e5248874d1dde0346976361a9aa1b4ce
Ben Dosch <Benjamindosch@gmail.com>
2020-11-05 21:01:35 +0000

    updates to header

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - returns the number of bits that need be change to 
+ * make the numbers the same
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int sum = 0;
+
+	while(n || m)
+	{
+		if ((n & 1) ^ (m & 1))
+			sum++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return (sum);
+}
