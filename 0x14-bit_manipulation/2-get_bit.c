8fd8ddaba55ba41c112dc3b18919524f601c30ff
Ben Dosch <Benjamindosch@gmail.com>
2020-11-05 19:38:35 +0000

    index - 1 shift

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 16)
 		return (-1);
-	n = n >> index;
+	if (index == 0)
+		reutrn (n & 1);
+	n = n >> index - 1;
 	return (n & 1);
 }
