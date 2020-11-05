0027eae4f70b4ffb73f588f23b5955da2db0dfef
Ben Dosch <Benjamindosch@gmail.com>
2020-11-05 21:06:13 +0000

    ; removed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  * Return: 1 on sucess, -1 on fail
  */
 
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
