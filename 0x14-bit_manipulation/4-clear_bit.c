dd45279c24396ccca06cec1804d854e558c0833f
Ben Dosch <Benjamindosch@gmail.com>
2020-11-05 20:27:18 +0000

    ready to test

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - temp
+ * @n: number
+ * @index: index
+ * Return new num or -1 if fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int base2 = 1, num = *n;
+	unsigned int i;
+
+	num >>= index;
+	if (index > 64)
+		return (-1);
+	if (num & 1)
+	{
+		for (i = 0; i < index; i++)
+			base2 *= 2;
+		*n -= base2;
+	}
+	return (*n);
+}
