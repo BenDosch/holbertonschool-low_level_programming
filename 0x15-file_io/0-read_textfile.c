9491a260a116bdfcfa2b8eda971d71ae4c47cb32
Ben Dosch <Benjamindosch@gmail.com>
2020-11-09 23:26:02 +0000

    0 ready to check

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rd, prt;
-	char buff[letters + 1];
+	int fd, check;
+	size_t i;
+	ssize_t rd = 0, prt = 0;
+	char *buf;
+
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 	rd = read(fd, buf, letters);
+
 	if (rd == -1)
+	{
+		free(buf);
 		return (0);
-	prt = write(STDOUT_FILENO, buff, letters);
-	if (prt == -1)
-		return (0);
+	}
+	for (i = 0; buf[i] && (i < letters); i++, prt++)
+	{
+		check = write(1, (buf + i), 1);
+		if (check == -1)
+		{
+			free(buf);
+			return (0);
+		}
+	}
+	if (!buf[i])
+		prt++;
+	free(buf);
 	close(fd);
 	return (prt);
 }
