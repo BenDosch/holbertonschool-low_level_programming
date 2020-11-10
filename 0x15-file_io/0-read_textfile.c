17a9e70ac696983a23465b38ba908f449b4ceaf4
Ben Dosch <Benjamindosch@gmail.com>
2020-11-10 02:45:24 +0000

    cleaned up aand used const

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, check;
 	ssize_t rd = 0;
-	char *buf;
+	char *buf = malloc(sizeof(char) * letters);
 
 
 	if (!filename)
@@ -19,24 +19,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 	rd = read(fd, buf, letters);
-
 	if (rd == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	check = write(1, buf, rd);
+	check = write(STDOUT_FILENO, buf, rd);
 	if (check == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	if (!buf[i])
-		prt++;
 	free(buf);
 	close(fd);
 	return (prt);
