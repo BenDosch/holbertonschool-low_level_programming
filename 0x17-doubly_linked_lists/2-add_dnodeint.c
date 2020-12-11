eb18c9e44cb28f8665634acede564ecedb8b1a20
Ben Dosch <Benjamindosch@gmail.com>
2020-12-11 19:21:11 +0000

    ready to test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,1 +1,25 @@
 #include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node containing an int at the start of a
+ * doublly linked list
+ * @head: Head of list
+ * @n: Number to store in Node
+ * Return: pointer to new node, NULL on fail
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (head)
+		new->next = *head;
+	*head = new;
+	return (new);
+	}
