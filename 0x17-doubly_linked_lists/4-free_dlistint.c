b4b60110ce9de47e0947489f0269123c05177dde
Ben Dosch <Benjamindosch@gmail.com>
2020-12-11 21:11:55 +0000

    ready to test

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * free_dlistint - frees a double list of type dlistint_t
+ * @head: Head of list
+ * Return: None
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head, *next;
+
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
