0abdbea5372d6202018904b294cf5593c47fb754
Ben Dosch <Benjamindosch@gmail.com>
2020-12-11 21:26:40 +0000

    ready to test

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+
+
+/**
+ * sum_dlistint - Returns the sum of the values store in a double linked list
+ * @head: header of list
+ * Return: Sum of node values
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
