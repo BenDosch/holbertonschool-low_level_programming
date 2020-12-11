a8727d8b91a5f9f23efa5b0481fe347d900f5ce8
Ben Dosch <Benjamindosch@gmail.com>
2020-12-11 19:52:57 +0000

    ready to test

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+
+
+/**
+ * dlistint_len - returns the len of a doublely linked list
+ * @h: header of list
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *current = (dlistint_t *)h;
+	size_t count = 0;
+
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
