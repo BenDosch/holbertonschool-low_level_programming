4c7f69175ce783ca176a7b3bb1da3efbf4997d45
Ben Dosch <Benjamindosch@gmail.com>
2020-12-11 21:20:30 +0000

    ready to test

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds the index-th node in a doubbly linked list
+ * @head: head of list
+ * @index: index to search for
+ * Return: pointer to node at index or NULL if no node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int i;
+
+	for (i = 0; i <= index && current; i++)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+	}
+	return (NULL);
+}
