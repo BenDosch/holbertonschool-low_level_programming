573f81fa4ee2fbe397a3387c97643590e4c07438
Ben Dosch <Benjamindosch@gmail.com>
2020-12-13 21:25:32 +0000

    adjusted when adding to end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	for (i = 0; i < idx; i++)
 	{
-		if (current == NULL && i == (idx - 1))
+		if (current->next == NULL && i == (idx - 2))
 		{
 			new = add_dnodeint_end(h, n);
 			return (new);
