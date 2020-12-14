da28513d66ff2f407cfabaf5e26952766d8c184f
Ben Dosch <Benjamindosch@gmail.com>
2020-12-14 01:38:20 +0000

    added NULL checks for head and *head

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,6 +13,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 	unsigned int i;
 
+	if (!head || !current)
+		return (-1);
 	for (i = 0; current != NULL && i <= index; i++)
 	{
 		if (i == index)
@@ -31,7 +33,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (1);
 		}
 		current = current->next;
-		printf("node: %d\n", current->n);
 	}
 	return (-1);
 }
