397f24b15d587db4de92e8242c46721b615ee3a4
Ben Dosch <Benjamindosch@gmail.com>
2020-12-18 03:20:48 +0000

    ready to test

---
+++ 0x18-dynamic_libraries/100-math_funcs.c
@@ -0,0 +1,59 @@
+/**
+ * add - returns the sum of a and b
+ * @a: first int
+ * @b: second int
+ * Return: result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns diffrence the of a and b
+ * @a: first int
+ * @b: second int
+ * Return: result of a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns the product of a and b
+ * @a: first int
+ * @b: second int
+ * Return: result of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - returns the result of the devision of a by b
+ * @a: first int
+ * @b: second int
+ * Return: result of a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder of a devided by b
+ * @a: first int
+ * @b: second int
+ * Return: remainder of a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
