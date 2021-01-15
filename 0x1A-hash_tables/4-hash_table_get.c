10c4f37f6e23121f0204734c8bf68d42b853498d
Ben Dosch <Benjamindosch@gmail.com>
2021-01-15 19:35:19 +0000

    added checks for invalid input

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t **bins, *cur;
 
+	if (ht == NULL || !(strcmp(key, "")) || key == NULL)
+		return (NULL)
+
 	index = key_index((const unsigned char *)key, ht->size);
 	bins = ht->array;
 
