8b00f97976d3a9686c62c29054fbb699d668266f
Ben Dosch <Benjamindosch@gmail.com>
2021-01-15 19:56:51 +0000

    task 6 added

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: hash table to use
+ *
+ * Return: None
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **bins, *cur, *last;
+	unsigned long int i;
+
+	bins = ht->array;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		for (cur = bins[i]; cur != NULL;)
+		{
+			last = cur;
+			cur = cur->next;
+			free(last->key);
+			free(last->value);
+			free(last);
+		}
+	}
+	free(bins);
+	free(ht);
+}
