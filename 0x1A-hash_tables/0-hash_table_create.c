c37a8efbca8fa85081c77f6d8836cfdb77f71c71
Ben Dosch <Benjamindosch@gmail.com>
2021-01-15 17:16:04 +0000

    betty fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - creates a hash table
+ * hash_table_create - creates a hash table
  *
  * @size: size of hash table
  *
@@ -15,13 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t*) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
