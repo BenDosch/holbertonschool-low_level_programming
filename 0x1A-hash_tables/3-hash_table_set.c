cafe7e281fb89daa95af917ff3527188046da67e
Ben Dosch <Benjamindosch@gmail.com>
2021-01-15 17:51:33 +0000

    task 3 added

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds and element to a hash table
+ *
+ * @ht: hash table
+ * @key: key of element to add
+ * @value: value of element to add
+ *
+ * Return: 1 on sucess, 0 on fail.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t **bins, *cur;
+	hash_node_t *new_ele;
+
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+		return 0;
+
+	new_ele = malloc(sizeof(hash_node_t*));
+	if (new_ele == NULL)
+		return 0;
+
+	index = key_index((const unsigned char*)key, ht->size);
+	bins = ht->array;
+	new_ele->key = strdup(key);
+	new_ele->value = strdup(value);
+	new_ele->next = NULL;
+
+	if (bins[index] == NULL)
+	{
+		bins[index] = new_ele;
+		return 1;
+	}
+	else
+	{
+		for (cur =  bins[index]; cur != NULL; cur = cur->next)
+		{
+			if (cur->key == new_ele->key)
+			{
+				cur->value = new_ele->value;
+				free (new_ele);
+				return 1;
+			}
+		}
+		new_ele->next = bins[index];
+		bins[index] = new_ele;
+	}
+
+	return 1;
+}
---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+	hash_table_t *ht;
+	char *key, *value;
+
+	ht = hash_table_create(1024);
+	key = "betty";
+	value = "holberton";
+	hash_table_set(ht, key, value);
+	printf("%s added at %s\n", value, key);
+	return (EXIT_SUCCESS);
+}
