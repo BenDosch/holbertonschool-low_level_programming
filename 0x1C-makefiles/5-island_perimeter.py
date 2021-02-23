dc76bdef0872c42e23c181a5b57a262f0d58cec9
Ben Dosch <Benjamindosch@gmail.com>
2021-02-23 23:50:49 +0000

    trying adding m.h to sources

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,24 +9,20 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i != 0:
-                    if not grid[i - 1][j]:
-                        perimiter += 1
+                if i != 0 and grid[i - 1][j]:
+                    pass
                 else:
                     perimiter += 1
-                if i != len(grid) - 1:
-                    if not grid[i + 1][j]:
-                        perimiter += 1
+                if i != len(grid) - 1 and grid[i + 1][j]:
+                    pass
                 else:
                     perimiter += 1
-                if j != 0:
-                    if not grid[i][j - 1]:
-                        perimiter += 1
+                if j != 0 and grid[i][j - 1]:
+                    pass
                 else:
                     perimiter += 1
-                if j != len(grid[i]) - 1:
-                    if not grid[i][j + 1]:
-                        perimiter += 1
+                if j != len(grid[i]) - 1 and grid[i][j + 1]:
+                    pass
                 else:
                     perimiter += 1
     return perimiter
