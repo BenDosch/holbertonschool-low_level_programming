e095f790ce234e299d0ab990cb8d33698e56896d
Ben Dosch <Benjamindosch@gmail.com>
2021-02-23 02:38:30 +0000

    first attempt

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+def island_perimeter(grid):
+    """Calculates the perimeter of an island"""
+    perimiter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:    
+                if i != 0:
+                    if not grid[i - 1][j]:
+                        perimiter += 1
+                else:
+                    perimiter += 1
+                if i != len(grid):
+                    if not grid[i + 1][j]:
+                        perimiter += 1
+                else:
+                    perimiter += 1
+                if j != 0:
+                    if not grid[i][j - 1]:
+                        perimiter += 1
+                else:
+                    perimiter += 1
+                if j != len(grid[i]):
+                    if not grid[i][j + 1]:
+                        perimiter += 1
+                else:
+                    perimiter += 1
+    return perimiter
