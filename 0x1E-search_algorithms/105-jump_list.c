222ae91c896b3d972563f5a1ca271d44d0ee2ad5
Ben Dosch <Benjamindosch@gmail.com>
2021-04-20 03:43:50 +0000

    Task 12 ready to check

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,5 +12,35 @@
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    /*code*/
+	size_t block = sqrt(size), i, j;
+	listint_t *curr = list, *next;
+
+	if (curr != NULL)
+	{
+		for (i = 0; i < size; i += block, curr = next)
+		{
+
+			for (j = 0, next = curr; j < block; j++, next = next->next)
+			{
+				if (next->next == NULL)
+					break;
+			}
+			printf("Value checked at index [%lu] = [%d]\n",
+				   next->index, next->n);
+			if (next->n >= value || next->next == NULL)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n",
+					   curr->index, next->index);
+				for (; curr != next; curr = curr->next)
+				{
+					printf("Value checked at index [%lu] = [%d]\n",
+						   curr->index, curr->n);
+					if (curr->n == value)
+						return (curr);
+				}
+				return (NULL);
+			}
+		}
+	}
+	return (NULL);
 }
