630d01c0fd26d5b740d6af881e26842a934a4cb4
Ben Dosch <Benjamindosch@gmail.com>
2021-04-20 04:33:30 +0000

    Task 13 ready to check

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,15 +1,75 @@
 #include "search_algos.h"
 
 /**
-* linear_skip - description
+* linear_list - function that searches for a value in a
+* sorted list of integers using the Linear search algorithm.
 *
-* @list: describe
-* @value: description
+* @start: Pointer to the starting node of the sorted linked list that
+* is the subject of search
+* @end: Ending node for search
+* @value: Value to find
 *
-* Return: value
+* Return: On sucess, pointer to the first node containing value
+* On failure, NULL.
+*/
+
+skiplist_t *linear_list(skiplist_t *start, skiplist_t *end, int value)
+{
+	skiplist_t *curr = start;
+
+	if (curr != NULL)
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				   start->index, end->index);
+		for (; curr->next != NULL && curr != end; curr = curr->next)
+		{
+			printf("Value checked at index [%lu] = [%d]\n",
+			   curr->index, curr->n);
+			if (curr->n == value)
+				return (curr);
+		}
+		printf("Value checked at index [%lu] = [%d]\n",
+			   curr->index, curr->n);
+		if (curr->n == value)
+			return (curr);
+	}
+	return (NULL);
+}
+
+/**
+* linear_skip - Function that searches for a
+* value in a sorted skip list of integers
+*
+* @list: Pointer to the sorted linked list that is the subject of search
+* @value: Value to find
+*
+* Return: On sucess, pointer to the first node containing value
+* On failure, NULL.
 */
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    /*code*/
+	skiplist_t *curr = list, *last;
+
+	while (curr != NULL)
+	{
+		if (curr->n == value)
+			return (curr);
+		else if (curr->express != NULL && curr->express->n > value)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				   curr->index, curr->express->index);
+			return (linear_list(curr, curr->express, value));
+		}
+		else if (curr->express == NULL)
+		{
+			for (last = curr; last->next != NULL; last = last->next)
+				continue;
+			return (linear_list(curr, last, value));
+		}
+		printf("Value checked at index [%lu] = [%d]\n",
+			curr->index, curr->n);
+		curr = curr->express;
+	}
+	return (NULL);
 }
