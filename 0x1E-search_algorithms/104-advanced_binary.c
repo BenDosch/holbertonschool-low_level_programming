8accaf3068fa85ff6432ba060d0b5a5611eb5594
Ben Dosch <Benjamindosch@gmail.com>
2021-04-20 20:43:05 +0000

    made it recursived like the requirements asked for

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,45 @@
 #include "search_algos.h"
 
 /**
+* recurse_binary - Function that searches for a value in
+* a sorted array of integers.
+*
+* @array: Pointer to the sorted array that is the subject of search
+* @size: Size of the array
+* @value: Value to find
+* @offset: Offset of index from origional array.
+*
+* Return: Return: On sucess, return the index of the first match of value.
+* On failure,  returns -1.
+*/
+
+int recurse_binary(int *array, size_t size, int value, size_t offset)
+{
+	size_t L = 0, M = 0, R = (size - 1), i;
+
+	if (array != NULL)
+	{
+		printf("offset: %lu   :", offset);
+		printf("Searching in array: %d", array[L]);
+		for (i = L + 1; i <= R; i++)
+			printf(", %d", array[i]);
+		printf("\n");
+		M = ((L + R) / 2);
+		if (L == R && array[L] == value)
+			return (R + offset);
+		else if (L == R)
+			return (-1);
+		else if (array[M] == value)
+			return (recurse_binary(array, (M + 1), value, offset));
+		else if (array[M] < value)
+			return (recurse_binary((array + (((int)M) + 1)), (size - (M + 1)), value, (offset + M + 1)));
+		else if (array[M] > value)
+			return (recurse_binary(array, M, value, offset));
+	}
+	return (-1);
+}
+
+/**
 * advanced_binary - Function that searches for a value in
 * a sorted array of integers.
 *
@@ -14,26 +53,5 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t L = 0, M = 0, R = (size - 1), i;
-
-	if (array != NULL)
-	{
-		while (L <= R)
-		{
-			printf("Searching in array: %d", array[L]);
-			for (i = L + 1; i <= R; i++)
-				printf(", %d", array[i]);
-			printf("\n");
-			M = ((L + R) / 2);
-			if (L == R)
-				return (R);
-			else if (array[M] == value)
-				R = M;
-			else if (array[M] < value)
-				L = (M + 1);
-			else if (array[M] > value)
-				R = (M - 1);
-		}
-	}
-	return (-1);
+	return (recurse_binary(array, size, value, 0));
 }
